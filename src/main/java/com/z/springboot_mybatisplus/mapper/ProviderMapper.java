65945ed985945e164abf8a1e0f27a7f5a01780a8
Zhang <2046179787@qq.com>
2023-12-07 15:19:53 +0800

    2023.12.7(二) 本次提交补充了分页

--- src/main/java/com/z/springboot_mybatisplus/mapper/ProviderMapper.java
+++ src/main/java/com/z/springboot_mybatisplus/mapper/ProviderMapper.java
@@ -8,12 +8,4 @@ import java.util.List;
 import java.util.Map;
 
 public interface ProviderMapper extends BaseMapper<Provider> {
-    List<Provider> findProvider(@Param("proName") String proName, @Param("proDesc") String proDesc);
-    Double findProviderBillPrice(Map<String,Object> map);
-    List<Provider> findAllProvider(@Param("proName")String proName,@Param("proDesc")String proDesc,@Param("proAddress")String proAddress);
-    int addProvider(Provider provider);
-    int delProviderById(Integer id);
-    int delProviderByIds(Integer[] list);
-    int updProvider(Provider provider);
-    Provider selectProviderById(Integer id);
 }
