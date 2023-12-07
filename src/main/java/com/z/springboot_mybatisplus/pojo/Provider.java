824f9112280560a12eebbf042510601ea19e15c3
Zhang <2046179787@qq.com>
2023-12-07 10:33:24 +0800

    2023.12.7提交

--- src/main/java/com/z/springboot_mybatisplus/pojo/Provider.java
+++ src/main/java/com/z/springboot_mybatisplus/pojo/Provider.java
@@ -1,6 +1,8 @@
 package com.z.springboot_mybatisplus.pojo;
 
+import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
@@ -20,29 +22,19 @@ import java.util.List;
 @Component
 @TableName("smbms_provider")
 public class Provider {
+    @TableId(type = IdType.AUTO)
     private Integer id;   //id
-    @TableField("proCode")
     private String proCode; //供应商编码
-    @TableField("proName")
     private String proName; //供应商名称
-    @TableField("proDesc")
     private String proDesc; //供应商描述
-    @TableField("proContact")
     private String proContact; //供应商联系人
-    @TableField("proPhone")
     private String proPhone; //供应商电话
-    @TableField("proAddress")
     private String proAddress; //供应商地址
-    @TableField("proFax")
     private String proFax; //供应商传真
-    @TableField("createdBy")
     private Integer createdBy; //创建者
-    @TableField("creationDate")
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private Timestamp creationDate; //创建时间
-    @TableField("modifyBy")
     private Integer modifyBy; //更新者
-    @TableField("modifyDate")
     private Date modifyDate;//更新时间
 
 
